0e7fbbb494d2511133bfc653811e7cf050c1c0d9
Stefany oliveira <desenvolvedoraweb@gmail.com>
2021-03-29 14:32:59 -0300

    Composer e Autoload PSR-4

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php
+    require_once "../vendor/autoload.php";
     echo "Olá mundo. Isso está funcionando no MVC";
-    ?>
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '66fb5f788659c82eddc5910acdfda6080002d36e',
+    'name' => 'vendorname/mvc',
+  ),
+  'versions' => 
+  array (
+    'vendorname/mvc' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '66fb5f788659c82eddc5910acdfda6080002d36e',
+    ),
+  ),
+);
